afb505052185bd820f6f854ffcc492f8bd574a3d
Martin-Louvard <martin.louvard@gmail.com>
2020-11-23 18:30:36 +0100

    Ajout de ispaid au cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_162148) do
+ActiveRecord::Schema.define(version: 2020_11_23_172455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_162148) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_paid", default: false
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
