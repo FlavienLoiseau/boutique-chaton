8da133c581df934d3d4620549c8ea357280a1af4
Martin-Louvard <martin.louvard@gmail.com>
2020-11-23 17:26:59 +0100

    Setup DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :addresses, dependent: :destroy
+  has_many :carts
+  has_one_attached :avatar
+
+  validates_presence_of :first_name, :last_name
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   devise_for :users
+  resources :products
+  root 'product#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
