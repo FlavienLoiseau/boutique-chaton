8280658780d571db57b4713cdc84c12140251c39
Mahmood Sharifi <mahmood.sharifi@icloud.com>
2020-11-24 16:02:07 +0100

    total of carts and sub-total of line items works

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,12 @@
 class Cart < ApplicationRecord
     belongs_to :user
     has_many :line_items
+
+    def total
+        total = 0
+        self.line_items.each do |line_item|
+            total += (line_item.quantity * line_item.product.price)
+        end
+        return total
+    end
 end
