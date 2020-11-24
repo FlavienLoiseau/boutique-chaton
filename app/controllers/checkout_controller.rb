2ff697219815cda89aeb87fa3bfd7a36f9def239
Martin-Louvard <martin.louvard@gmail.com>
2020-11-24 15:04:17 +0100

    pb resolu stripe

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,30 +1,26 @@
 class CheckoutController < ApplicationController
     def create
+        product = Product.find(params[:id])
 
+        if product.nil?
+          redirect_to root_path
+          return
+        end
 
-    
-        Stripe.api_key = 'sk_test_33HvkIMjnV9FWwvqoFlibMGs00HNc7nma3'
+    Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
         @session = Stripe::Checkout::Session.create({
             payment_method_types: ['card'],
             line_items: [{
-              price_data: {
-                currency: 'eur',
-                product_data: {
-                  name: "test",
-                },
-                unit_amount: 5000,
-              },
-              quantity: 1,
-            }],
-            mode: 'payment',
-            # For now leave these URLs as placeholder values.
-            #
-            # Later on in the guide, you'll create a real success page, but no need to
-            # do it yet.
-            success_url: 'https://example.com/success',
-            cancel_url: 'https://example.com/cancel',
-          })
+                    name: product.title,
+                    description: product.description,
+                    amount: (product.price* 100).to_i,
+                    currency: 'eur',
+                    quantity: 1
+                  }],
+            success_url: checkout_success_url,
+            cancel_url: checkout_cancel_url,
+        })
 
 
 
