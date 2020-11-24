673539fbb60fd9ca96a70154e5b1a24de21362c9
FlavienLoiseau <flavien.loiseau@edu.escp.eu>
2020-11-24 11:08:23 +0100

    add front components

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :carts
   devise_for :users
   resources :products
+  get 'home/index'
   root 'product#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
