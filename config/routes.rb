7b43775b8ae1c6e099abe989317001b26ac54d19
josephineremy <jremy2@audencia.com>
2020-11-25 15:51:19 +0100

    new static pages

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
   end
 
   get 'home/index'
+  get 'home/about', as: 'about'
+  get 'home/contact', as: 'contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
