49f7421cd9711056f318494e687d3b0ad309ee22
Mahmood Sharifi <mahmood.sharifi@icloud.com>
2020-11-25 14:56:40 +0100

    address can be added from profile edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,14 @@ class User < ApplicationRecord
   has_one :address, dependent: :destroy
   has_many :carts
   has_one_attached :avatar
-
+  accepts_nested_attributes_for :address
   validates_presence_of :first_name, :last_name
+
+  def address_attributes=(address_attributes)
+    address_attributes.each do |i, address_attributes|
+      self.address.build(address_attributes)
+    end 
+    
+  end
 
 end
