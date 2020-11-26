40c8b4fece90d2955e2c8e29490ad281f55c6bc5
FlavienLoiseau <flavien.loiseau@edu.escp.eu>
2020-11-26 15:33:18 +0100

    emails for admins

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,17 @@ class UserMailer < ApplicationMailer
 
   def checkout_success(user)
     @cart = user.carts.last
-
     mail(to: user.email, subject: "Boutique Chaton - Votre commande n°#{@cart.id}/#{user.id}")
+  end
 
+  def new_sale(user)
+    @cart = user.carts.last
+    admins_emails = []
+    User.all.each do |u|
+      if u.is_admin
+        admins_emails << u.email
+      end
+    end
+    mail(to: admins_emails, subject: "Nouvelle commande sur Boutique Chaton !")
   end
 end
